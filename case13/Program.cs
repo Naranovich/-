fbb93486088d71639858bc506386fc38d25588d4
Naranovich <nik.naranovich@mail.ru>
2022-11-17 01:21:34 +0300

    case13

---
+++ case13/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.WriteLine("Введите трехзначное число:");
+int number = Convert.ToInt32(Console.ReadLine());
+string numberS = Math.Abs(number).ToString();
+Console.WriteLine(numberS.Length == 3 ? $"{number} -> {numberS[1]}" : "Вы ввели не трехзначное число");
