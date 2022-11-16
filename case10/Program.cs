53716d8c4eb6212c549d6131b55ae76f8f160824
Naranovich <nik.naranovich@mail.ru>
2022-11-17 01:44:09 +0300

    case10

---
+++ case10/Program.cs
@@ -0,0 +1,6 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"случайное число {number}");
+string numberS = Math.Abs(number).ToString();
+Console.WriteLine(numberS.Length == 3 ? $"{number} -> {numberS[1]}" : "результат");
