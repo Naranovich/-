0aeac78ef0b0a2eb7204af06944de60ea7f9d5db
Naranovich <nik.naranovich@mail.ru>
2022-11-17 02:28:33 +0300

    case 15

---
+++ case15/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите цифру обозначающую день недели:");
+int num = Convert.ToInt32(Console.ReadLine());
+string numberS = (num > 0 && num < 8) 
+    ? (num == 6 || num == 7) ? "Выходной" : "рабочий день"
+    : "не день недели";
+Console.WriteLine($"{num} -> {numberS}");
