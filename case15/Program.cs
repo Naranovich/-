8507665f8bf74b67337eaf85a6e161ab74319d18
Naranovich <nik.naranovich@mail.ru>
2022-11-17 02:32:19 +0300

    case 15

--- case15/Program.cs
+++ case15/Program.cs
@@ -2,5 +2,5 @@
 int num = Convert.ToInt32(Console.ReadLine());
 string numberS = (num > 0 && num < 8) 
     ? (num == 6 || num == 7) ? "Выходной" : "рабочий день"
-    : "не день недели";
+    : "в неделе 7 дней";
 Console.WriteLine($"{num} -> {numberS}");
