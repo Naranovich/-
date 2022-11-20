3fd5fd74864cee4eeb88e2ff81b21aa803cf7048
Naranovich <nik.naranovich@mail.ru>
2022-11-21 01:07:49 +0300

    case19

---
+++ case19/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 19
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.WriteLine("Введите число:");
+string num = Console.ReadLine();
+int len = num.Length;
+if (len == 5){
+    if(num[0] == num[4] && num[1] == num[3]){
+        Console.WriteLine($"{num} - полиндром");
+    }
+    else{
+        Console.WriteLine($"{num} - НЕ полиндром");
+    }
+}
+else{
+    Console.WriteLine($"{num} - не пятизначное число");
+}
