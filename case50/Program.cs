0c8bf1a24c2a9fb2e57bf567e659ed4872f2e5be
Naranovich <nik.naranovich@mail.ru>
2022-12-04 19:10:40 +0300

    case50

--- case50/Program.cs
+++ case50/Program.cs
@@ -3,24 +3,39 @@ Console.Write("Введите m: ");
 int m = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите n: ");
 int n = Convert.ToInt32(Console.ReadLine());
-
-double[,] numbers = new double[m, n];
+int[,] numbers = new int[n, m];
 ArrayRandomNumbers(numbers);
+
+
+for (int j = 0; j < numbers.GetLength(1); j++)
+{
+    double a = 0;
+    for (int i = 0; i < numbers.GetLength(0); i++)
+    {
+        a = (a + numbers[i, j]);
+    }
+    a = a / n;
+    Console.Write(a + "; ");
+}
+Console.WriteLine();
 PrintArray(numbers);
 
-void ArrayRandomNumbers(double[,] array)
+
+
+void ArrayRandomNumbers(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
         {
-            array[i, j] = Convert.ToDouble(new Random().Next(-10, 10)) / 10;
+            array[i, j] = new Random().Next(0, 10);
         }
     }
 }
 
-void PrintArray(double[,] array)
+void PrintArray(int[,] array)
 {
+
     for (int i = 0; i < array.GetLength(0); i++)
     {
         Console.Write("[ ");
@@ -29,6 +44,6 @@ void PrintArray(double[,] array)
             Console.Write(array[i, j] + " ");
         }
         Console.Write("]");
-        Console.WriteLine();
+        Console.WriteLine("");
     }
 }
