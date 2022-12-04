28d3862f992813222ce805e04d85b94b35565fbf
Naranovich <nik.naranovich@mail.ru>
2022-12-04 19:30:00 +0300

    case52

---
+++ case52/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+Console.WriteLine("Введите размерность двумерного массива m x n");
+Console.Write("Введите m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] numbers = new int [10,10];
+ArrayRandomNumbers(numbers);
+
+if (m < numbers.GetLength(0) && n < numbers.GetLength(1)) Console.WriteLine(numbers[m, n]);
+
+else Console.WriteLine($"{m}{n} -> такого числа в массиве нет");
+
+PrintArray(numbers);
+void ArrayRandomNumbers(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+        {        
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array [i,j] = new Random().Next(1, 10);
+            }   
+        }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        Console.Write("[ ");
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i,j] + " ");
+        }   
+        Console.Write("]");
+        Console.WriteLine(""); 
+    }
+}
+
