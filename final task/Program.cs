94c9ac3e6a0b8456206d9585a642152171fe2be5
Naranovich <nik.naranovich@mail.ru>
2022-12-24 15:03:18 +0300

    метод на вывод

---
+++ final task/Program.cs
@@ -0,0 +1,25 @@
+﻿string[] arr1 = new string[4] {"2", "hello", "world", ":)"};
+string[] arr2 = new string[arr1.Length];
+int count = 0;
+void SecondArrayWithIF(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+SecondArrayWithIF(arr1, arr2);
+PrintArray(arr2);
